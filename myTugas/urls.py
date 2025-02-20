949a8abd5f099b83f47c2fc4c54808793ba619ba
Fadel Fitrah <fansurifadelfitra@gmail.com>
2025-02-20 13:23:29 +0700

    Perubahan UI

--- myTugas/urls.py
+++ myTugas/urls.py
@@ -19,9 +19,8 @@ from django.urls import path
 from tasks import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', views.index, name='index'),
-    path('complete/<int:task_id>/', views.complete_task, name='complete_task'),
+    path('edit/<int:task_id>/', views.edit_task, name='edit_task'),
     path('delete/<int:task_id>/', views.delete_task, name='delete_task'),
 ]
 
